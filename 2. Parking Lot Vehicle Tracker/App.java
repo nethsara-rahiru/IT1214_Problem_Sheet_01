9ec9a733437b1082e21ad657ea7fe70a38f66399
nethsara-rahiru <rahiru123@gmail.com>
2025-05-20 19:43:07 +0000

    Update 006

--- 2. Parking Lot Vehicle Tracker/App.java
+++ 2. Parking Lot Vehicle Tracker/App.java
@@ -26,8 +26,110 @@ class Vehicle{
 
 }
 
+class ParkingLot{
+    int maxVehicle = 5;
+    Vehicle[] vehicle = new Vehicle[maxVehicle];
+
+    void newVehicle(){
+        for (int i = 0; i < maxVehicle; i++ ){
+            if(Vehicle[i] == null){
+
+                Scanner scan = new Scanner(System.in);
+
+                vehicle[i] = new Vehicle();
+                System.out.println("Please enter the License Plate");
+                vehicle[i].setLicensePlate(scan.nextLine());
+                System.out.println("Please enter the Owner NAme");
+                vehicle[i].setOwnerName(scan.nextLine());
+                System.out.println("Please enter the Hours Parked");
+                vehicle[i].setHoursParked(Integer.parseInt(scan.nextLine()));
+            }
+        }
+
+        void showVehicle(){
+
+            System.out.println("Slot Details");
+            System.out.println("-------------------------------------");
+
+            for (int i = 0; i < maxVehicle; i++ ){
+                if (Vehicle[i] != null){
+
+                    // Complete this
+                    System.out.println("Slot Details");
+                    vehicle[i] = new Vehicle();
+                    System.out.println("Please enter the License Plate");
+                    vehicle[i].setLicensePlate(scan.nextLine());
+                    System.out.println("Please enter the Owner NAme");
+                    vehicle[i].setOwnerName(scan.nextLine());
+                    System.out.println("Please enter the Hours Parked");
+                    vehicle[i].setHoursParked(Integer.parseInt(scan.nextLine()));
+                }
+            }
+        }
+
+        void removeVehicle(){
+
+            Scanner scan = new Scanner();
+            int numP = Integer.parseInt(scan.nextLine());
+
+            for (int i = 0; i < maxVehicle; i++ ){
+                if(Vehicle[i] == null){
+                }
+                else{
+                    if(Vehicle[i].getLicensePlate == numP){
+                        Vehicle[i].setLicensePlate = "";
+                        Vehicle[i].setOwnerName = "";
+                        Vehicle[i].setHoursParked = 0;
+                        System.out.println("Vehicle is sucssesfully removed");
+
+                    }
+                }
+            }
+        }
+    }
+}
+
 class App{
     public static void main(String[] args){
 
+        int system = 1;
+
+        Scanner scan = new Scanner(System.in);
+        ParkingLot pl = new ParkingLot();
+        
+        while(system == 1){
+            System.out.println("--------------------------------------------");
+            System.out.println("Welcome to the Parking App")
+            System.out.println("--------------------------------------------");
+            System.out.println("");
+            System.out.println("Please select a option...");
+            System.out.println(" 1 - Add a new Vehicle");
+            System.out.println(" 2 - Remove a new Vehicle");
+            System.out.println(" 3 - Display slots");
+            System.out.println(" 4 - exit");
+
+            input = Integer.parseInt(scan.nextLine());
+
+            switch (input) {
+                case 1:
+                    pl.newVehicle();
+                    break;
+            
+                case 2:
+                    pl.removeVehicle();
+                    break;
+            
+                case 3:
+                    
+                    break;
+            
+                case 4:
+                    system = 0;
+                    break;
+            
+                default:
+                    break;
+            }
+        }
     }
 }
