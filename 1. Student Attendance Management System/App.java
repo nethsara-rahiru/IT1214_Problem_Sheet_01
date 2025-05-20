3be43939f1062982b561bf16120ace541871baba
nethsara-rahiru <rahiru123@gmail.com>
2025-05-20 16:53:43 +0000

    Update 003

--- 1. Student Attendance Management System/App.java
+++ 1. Student Attendance Management System/App.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 class Student{
-    private int studentId;
-    private String name;
-    private int daysAttended;
+    private int studentId = 0;
+    private String name = "";
+    private int daysAttended = 0;
 
     void setData(int studentId, String name, int daysAttended){
         this.studentId = studentId;
@@ -18,30 +20,138 @@ class Student{
         this.daysAttended = daysAttended;
     }
 
-    int getStudentID{
+    int getStudentID(){
         return studentId;
     }
-    String getName{
+    String getName(){
         return name;
     }
-    int getDaysAttended{
+    int getDaysAttended(){
         return daysAttended;
     }
 }
 
 class Classroom{
-        Student[] student = new Student[10];
+        int studentLimit = 10;
+        Student[] student = new Student[studentLimit];
         int counter = 0;
 
-        void addNewStudent(String name){
-            if (counter < 10 - 1){
-                student[counter] = name;    
+        void updateAttendense(){
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Please enter student ID need to update");
+            int id = Integer.parseInt(scan.nextLine());
+
+            for(int i = 0; i < studentLimit; i++){
+                if(student[i] == null){
+
+                }
+                else{
+                    if(i == 9){
+                        System.out.println("Student is not registered");
+                    }
+                    else{
+                        if(student[i].getStudentID() == id){
+                            System.out.println("Please enter the days attended");
+                            int day = Integer.parseInt(scan.nextLine());
+                            student[i].setDaysAttended(day);
+                            System.out.println("Please enter the days Sucsessfuly Updated");
+                            break;
+                        }   
+                    }
+                }
+            }
+        }
+
+        void showData(){
+
+            System.out.println("Student Report");
+            System.out.println("---------------------------------------------");
+            
+            for (int i = 0; i < studentLimit; i++){
+                if(student[i] == null){
+                    System.out.println("null");
+                }
+                else{
+                    System.out.println("");
+                    System.out.print("ID    : ");
+                    System.out.println(student[i].getStudentID());
+                    System.out.print("Name  : ");
+                    System.out.println(student[i].getName());
+                    System.out.print("Days  : ");
+                    System.out.println(student[i].getDaysAttended());
+                    System.out.println("");
+                }
+            }
+        }
+
+        void addNewStudent(){
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Please enter student ID");
+            int id = Integer.parseInt(scan.nextLine());
+
+            System.out.println("Please enter student name");
+            String stdName = scan.nextLine();
+
+            System.out.println("Please enter the days attended");
+            int doa = Integer.parseInt(scan.nextLine());
+
+            for (int i = 0 ; i < studentLimit; i++ ){
+                if ( student[i] == null ){
+                    student[i] = new Student();
+                    student[i].setStudentID(id);
+                    student[i].setName(stdName);
+                    student[i].setDaysAttended(doa);
+                    System.out.println("Data saved in posistion " + i );
+                    
+                }
             }
         }
 }
 
 class App{
     public static void main(String[] args){
+            int system = 1;
 
+            while(system == 1){
+            System.out.println("---------------------------------------------");
+            System.out.println("Welcome to the Classroom App");
+            System.out.println("---------------------------------------------");
+            System.out.println("");
+            System.out.println("Please select an input ...");
+            System.out.println("   1 - Add a new student");
+            System.out.println("   2 - Update Attendense");
+            System.out.println("   3 - Student report");
+            System.out.println("   4 - Exit");
+            System.out.println("");
+
+            Scanner scan = new Scanner(System.in);
+            Classroom classroom = new Classroom();
+
+            String input = scan.nextLine();
+            switch (input) {
+                case "1":
+                    classroom.addNewStudent();
+                    break;
+            
+                case "2":
+                    classroom.updateAttendense();
+                    break;
+            
+                case "3":
+                    classroom.showData();
+                    break;
+            
+                case "4":
+                    system = 0;
+                    break;
+            
+                default:
+                    System.out.println("Please enter a valid input ");
+                    break;
+            }
+        }
     }
 }
